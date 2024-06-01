cf5369a8979d68b6c318c7c4c6a741eca927ca25
kyskys <162934416+yonsoo7@users.noreply.github.com>
2024-06-02 00:12:25 +0900

    Update valid-date.sh

--- valid-date.sh
+++ valid-date.sh
@@ -7,20 +7,3 @@ if [ $# -ne 3 ]; then
   exit 1
 fi
 
-convert_month() {
-    case "$1" in
-        "Jan"|"1"|"January") echo "Jan";;
-        "Feb"|"2"|"February") echo "Feb";;
-        "Mar"|"3"|"March") echo "Mar";;
-        "Apr"|"4"|"April") echo "Apr";;
-        "May"|"5") echo "May";;
-        "Jun"|"6"|"June") echo "Jun";;
-        "Jul"|"7"|"July") echo "Jul";;
-        "Aug"|"8"|"August") echo "Aug";;
-        "Sep"|"9"|"September") echo "Sep";;
-        "Oct"|"10"|"October") echo "Oct";;
-        "Nov"|"11"|"November") echo "Nov";;
-        "Dec"|"12"|"December") echo "Dec";;
-        *) echo "Invalid";;
-    esac
-}

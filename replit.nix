dbb1a00c536a5093e939bc30f09935e226fd084e
kyskys <162934416+yonsoo7@users.noreply.github.com>
2024-06-03 05:26:41 +0000

    Commit message

--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
   deps = [
+    pkgs.nano
     pkgs.vim
     pkgs.bashInteractive
     pkgs.nodePackages.bash-language-server
--- valid-date.sh
+++ valid-date.sh
@@ -8,3 +8,20 @@ if [ ! $# = 3 ]; then
   exit 1
 fi
 
+case "${month,,}" in
+  jan|january|1) echo "Jan" ;;
+  feb|february|2) echo "Feb" ;;
+  mar|march|3) echo "Mar" ;;
+  apr|april|4) echo "Apr" ;;
+  may|5) echo "May" ;;
+  jun|june|6) echo "Jun" ;;
+  jul|july|7) echo "Jul" ;;
+  aug|august|8) echo "Aug" ;;
+  sep|september|9) echo "Sep" ;;
+  oct|october|10) echo "Oct" ;;
+  nov|november|11) echo "Nov" ;;
+  dec|december|12) echo "Dec" ;;
+  *) echo "Invalid month"; exit 1 ;;
+  esac
+
+
